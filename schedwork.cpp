b0b0cdfe3e6d71c8add2b0e57e10f351a2658590
Eric Huang <ehuang97@usc.edu>
2024-04-16 22:50:45 +0000

    hw5 commit 1

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,11 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, 
+                 DailySchedule& sched, vector<int>& shiftsWorking, int dayIndex, int shiftIndex);
 
+bool isValid(const AvailabilityMatrix& avail, size_t maxShifts, const DailySchedule& sched, 
+             const vector<int>& shiftsWorking, int dayIndex, int shiftIndex);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -32,14 +36,59 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if(avail.size() == 0){
         return false;
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    for (size_t i = 0; i < avail.size(); ++i) {
+        vector<Worker_T> day(dailyNeed);
+        sched.push_back(day);
+        for (size_t j = 0; j < dailyNeed; ++j) {
+				sched[i][j] = INVALID_ID;
+		}
+	}
+    vector<int> shifts((int)avail[0].size(), 0);
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, 0, 0);
 }
 
+bool scheduleHelper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, 
+                 DailySchedule& sched, vector<int>& shiftsWorking, int dayIndex, int shiftIndex) {
+    if (dayIndex == avail.size()) {
+        return true;
+    }
+
+    if (shiftIndex == dailyNeed) {
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsWorking, dayIndex + 1, 0);
+    }
+
+    for (Worker_T worker = 0; worker < avail[dayIndex].size(); ++worker) {
+        sched[dayIndex][shiftIndex] = worker;
+        shiftsWorking[worker]++;
+
+        if (isValid(avail, maxShifts, sched, shiftsWorking, dayIndex, shiftIndex)) {
+            if (scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsWorking, dayIndex, shiftIndex + 1)) {
+                return true;
+            }
+        }
+
+        sched[dayIndex][shiftIndex] = INVALID_ID;
+        shiftsWorking[worker]--;
+    }
+
+    return false;
+}
+
+bool isValid(const AvailabilityMatrix& avail, size_t maxShifts, const DailySchedule& sched, 
+             const vector<int>& shiftsWorking, int dayIndex, int shiftIndex) {
+    Worker_T worker = sched[dayIndex][shiftIndex];
+
+    for (int i = 0; i < shiftIndex; ++i) {
+        if (sched[dayIndex][i] == worker) {
+            return false;
+        }
+    }
+
+    if (!avail[dayIndex][worker] || shiftsWorking[worker] > maxShifts) return false;
+    return true;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+set<string> generateWords(string in, const string floating, int pos);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +21,53 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+  set<string> words = generateWords(in, floating, 0);
 
+  set<string> answer;
+  for (set<string>::iterator it = words.begin(); it != words.end(); ++it) {
+    // if the word exists in the set
+    if (dict.find(*it) != dict.end()) answer.insert(*it);
+  }
+  return answer;
 }
 
 // Define any helper functions here
+set<string> generateWords(string in, const string floating, int pos) {
+    set<string> words = {};
+
+    if (!in.length()) return words;
+
+    unsigned int count = 0;
+    for (unsigned int i = 0; i < in.length(); i++) {
+      if (in[i] == '-') ++count;
+
+    }
+    if (count == 0) {
+      words.insert(in);
+      return words;
+    }
+    
+    if (in[pos] == '-') {
+      for (unsigned int i = 0; i < floating.length(); i++) {
+        in[pos] = floating[i];
+        string temp = floating.substr(0, i) + floating.substr(i+1);
+        set<string> wordsTemp = generateWords(in, temp, pos+1);
+        words.insert(wordsTemp.begin(), wordsTemp.end());
+        in[pos] = '-';
+      }
+    
+      if (floating.length() < count) {
+        for (char c = 'a'; c <= 'z'; ++c) {
+            in[pos] = c;
+            set<string>wordsTemp = generateWords(in, floating, pos+1);
+            words.insert(wordsTemp.begin(), wordsTemp.end());
+            in[pos] = '-';
+        }
+      }
+    }
+    else {
+      set<string> wordsTemp = generateWords(in, floating, pos+1);
+      words.insert(wordsTemp.begin(), wordsTemp.end());
+    }
+  return words;
+}
